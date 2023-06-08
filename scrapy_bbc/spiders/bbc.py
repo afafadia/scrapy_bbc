f48449a397505918cd577ea5e546a5579c3aab3b
Aniket Aryamane <aniket.aryamane@fafadiatech.com>
2023-06-08 15:07:29 +0530

    Adding Images

--- scrapy_bbc/spiders/bbc.py
+++ scrapy_bbc/spiders/bbc.py
@@ -3,6 +3,11 @@ from urllib.parse import urlparse
 import scrapy
 
 
+def map_urls(image_src, image_data_src):
+    image_data_src = image_data_src.replace("{width}", "144") if "{width}" in image_data_src else image_data_src
+    return f"{image_data_src}" if "data" in image_src else f"{image_src}"
+
+
 class BBCSpider(scrapy.Spider):
     name = "bbc"
     allowed_domains = ["bbc.com"]
@@ -16,18 +21,26 @@ class BBCSpider(scrapy.Spider):
         return cleaned_urls
 
     def parse(self, response):
-        articles = self.remove_domain(response.css("a.media__link::attr(href)").getall())
-        tags = response.css("a.media__tag::text").getall()
+        articles = self.remove_domain(response.css("a.media__link ::attr(href)").getall())
+        tags = response.css("a.media__tag ::text").getall()
+        images_src = response.css(".media-list__item .responsive-image img ::attr(src)").getall()
+        images_data_src = response.css(".media-list__item .responsive-image ::attr(data-src)").getall()
+        valid_images = map(map_urls, images_src, images_data_src)
         home_page_titles = [title.strip() if title else "" for title in response.css("a.media__link::text").getall()]
 
-        for article, home_page_title, tag in zip(articles, home_page_titles, tags):
+        for i, (article, home_page_title, image, tag) in enumerate(
+            zip(articles, home_page_titles, valid_images, tags)
+        ):
             yield response.follow(
                 article,
                 callback=self.parse_article,
                 meta={
-                    "tag": tag,
+                    "i": i,
                     "home_page_title": home_page_title,
+                    "tag": tag,
+                    "image": image,
                 },
+                dont_filter=True,
             )
 
     def parse_article(self, response):
@@ -39,13 +52,14 @@ class BBCSpider(scrapy.Spider):
         home_page_title = response.meta["home_page_title"]
 
         data = {}
+        data["i"] = response.meta["i"]
 
         if page_title != home_page_title:
             data["home_page_title"] = home_page_title
 
         data_dict = {
             "page_title": page_title.strip() if page_title else "",
-            "image": "aniket",
+            "image": response.meta["image"],
             "url": response.url,
             "tag": response.meta["tag"],
         }
